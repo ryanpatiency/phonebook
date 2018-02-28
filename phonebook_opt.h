34277ddf0a8bf41fc67621ad4ee733a67f6372b0
ryanpatiency <ryanpatiency@gmail.com>
2018-02-28 10:05:54 +0800

    Shrink the size of each node

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -8,15 +8,15 @@
 // #define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
+//    char firstName[16];
+//    char email[16];
+//    char phone[10];
+//    char cell[10];
+//    char addr1[16];
+//    char addr2[16];
+//    char city[16];
+//    char state[2];
+//    char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
